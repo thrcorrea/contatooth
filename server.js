0becdb61b5b6452fec515eaadc647fc1de1311d4
thrcorrea <thales.rcorrea@gmail.com>
2016-09-26 08:30:50 -0300

    teste

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var config = require('./config/config')();
 require('./config/passport.js')();
 require('./config/database.js')(config.db);
 
+
 http.createServer(app).listen(app.get('port'), function(){
     console.log('Express Server Escutando na porta ' + app.get('port'));
 });
