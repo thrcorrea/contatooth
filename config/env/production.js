9d422d3555b3d007e92156b54e1ccfbc411f1b3b
Thales Correa <thales.rcorrea@gmail.com>
2016-10-02 22:22:06 -0300

    teste

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,7 @@ module.exports = function(){
   db: process.env.OPENSHIFT_MONGODB_DB_URL + 'contatooth',
   clientID: process.env.CLIENT_ID,
   clientSecret:  process.env.CLIENT_SECRET,
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  address: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  port: process.env.OPENSHIFT_NODEJS_PORT,
+  address: process.env.OPENSHIFT_NODEJS_IP,
   domain: process.env.OPENSHIFT_APP_DNS
 };
