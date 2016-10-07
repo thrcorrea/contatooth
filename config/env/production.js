5395bc8927840b75a5296df532b9629bbdf634de
Thales Correa <thales.rcorrea@gmail.com>
2016-10-06 21:38:30 -0300

    ajustado produtction

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: 'production',
-	db: process.env.OPENSHIFT_MONGODB_DB_URL + 'contatooh',
+	db: process.env.OPENSHIFT_MONGODB_DB_URL + 'contatooth',
 	clientID: process.env.CLIENT_ID,
 	clientSecret: process.env.CLIENT_SECRET,
 	port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
